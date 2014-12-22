79368581a8059ef5451f0e47bae7aec613313c7f
annajerofejeva <jerofejeva.anna@yahoo.com>
2014-12-22 16:30:32 +0000

    Can translate hundrets

--- UnitTests/Test.cs
+++ UnitTests/Test.cs
@@ -14,14 +14,25 @@ namespace UnitTests
 			Assert.AreEqual ("I", RomanNumerals.TranslateUnits (1));
 			Assert.AreEqual ("V", RomanNumerals.TranslateUnits (5));
 			Assert.AreEqual ("", RomanNumerals.TranslateUnits (10));
+			Assert.AreEqual ("III", RomanNumerals.TranslateUnits (13));
 		}
 
 		[Test ()]
-		public void TestingTranslatingTenths ()
+		public void TestingTranslatingTens ()
 		{
-			Assert.AreEqual ("X", RomanNumerals.TranslateTenths (10));
-			Assert.AreEqual ("L", RomanNumerals.TranslateTenths (55));
-			Assert.AreEqual ("", RomanNumerals.TranslateTenths (100));
+			Assert.AreEqual ("X", RomanNumerals.TranslateTens (10));
+			Assert.AreEqual ("L", RomanNumerals.TranslateTens (55));
+			Assert.AreEqual ("", RomanNumerals.TranslateTens (100));
+			Assert.AreEqual ("X", RomanNumerals.TranslateTens (514));
+		}
+
+		[Test ()]
+		public void TestingTranslatingHundrets ()
+		{
+			Assert.AreEqual ("C", RomanNumerals.TranslateHundrets (100));
+			Assert.AreEqual ("D", RomanNumerals.TranslateHundrets (555));
+			Assert.AreEqual ("", RomanNumerals.TranslateHundrets (1000));
+			Assert.AreEqual ("DC", RomanNumerals.TranslateHundrets (1698));
 		}
 	}
 }
--- roman-numerals-csharp/Program.cs
+++ roman-numerals-csharp/Program.cs
@@ -8,7 +8,8 @@ namespace romannumeralscsharp
 		public static void Main ()
 		{
 			Console.WriteLine(TranslateUnits (2));
-			Console.WriteLine(TranslateTenths (20));
+			Console.WriteLine(TranslateTens (20));
+			Console.WriteLine(TranslateHundrets (200));
 		}
 
 		public static string TranslateUnits (int num)
@@ -17,10 +18,16 @@ namespace romannumeralscsharp
 			return units [num % 10];
 		}
 
-		public static string TranslateTenths (int num)
+		public static string TranslateTens (int num)
 		{
-			string[] tenths = new string[10] {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
-			return tenths[(num % 100) / 10];
+			string[] tens = new string[10] {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+			return tens[(num % 100) / 10];
+		}
+
+		public static string TranslateHundrets (int num)
+		{
+			string[] hundrets = new string[10] {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+			return hundrets[(num % 1000) / 100];
 		}
 	}
 }
